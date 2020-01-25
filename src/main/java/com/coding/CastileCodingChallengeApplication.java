d4c7217482940963ddfdead2e5137a1944b9c599
Eric Arden <ardeneric77@gmail.com>
2020-01-25 19:07:52 +0000

    Initial commit. Package structure created

---
+++ src/main/java/com/coding/CastileCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CastileCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CastileCodingChallengeApplication.class, args);
+	}
+
+}
